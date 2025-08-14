b51acb7b0a18b8a3ea0f2485d09e4a262d8ad652
Khushi Rai <khushirai24102003@gmail.com>
2025-08-14 12:15:06 +0530

    Initial commit with demo files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test Case Generator Demo Repo
+This repository contains sample Python, Java, and React code for testing the Test Case Generator application.
---
+++ react-app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Test Generator</h1>
+    </div>
+  );
+}
+
+export default App;
